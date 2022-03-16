2d75e040fea13c9068344b8259554447ddaf40a8
Pavel <97126500+pashicop@users.noreply.github.com>
2022-03-17 00:10:02 +0300

    Task 1 update

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+#!/usr/bin/env python3
 
+import os
 
-
-
-if __name__ == '__main__':
-    a = 1
-    b = '2'
-    c = a + int(b)
-    # c = str(a) + b
-    print(c)
+bash_command = ["cd ~/4", "git status"]
+result_os = os.popen(' && '.join(bash_command)).read()
+dir = os.popen("cd").read()
+print(dir)
+#is_change = False
+prepare_result = []
+for result in result_os.split('\n'):
+    if result.find('modified') != -1:
+        prepare_result.append(result.replace('\tmodified:   ', ''))
+for name in prepare_result:
+    print(name)
